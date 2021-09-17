7c2c2286d176e54fefb053f41534fc011dbbd36f
carlinjshaw <carlinjshaw@gmail.com>
2021-09-17 09:03:16 -0400

    form has validation for email and message box

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -5,8 +5,10 @@ import {useState} from 'react';
 
 function Connect(props) {
   const [textValue, setTextValue] = useState('hi')
-  const [emailValue, setEmailValue] = useState('hi')
+  const [emailValue, setEmailValue] = useState('email@aol.com')
 
+
+//this gives the setter the value of whatever is typed in the form
 const onChangeHandler = (event) => {
    if (event.target.id === "email") {
      setEmailValue(event.target.value)
@@ -15,9 +17,10 @@ const onChangeHandler = (event) => {
    }
 }
 
+//this validates that the email is an email right after someone clicks outside of the email entry
 const onBlurHandler = (event) => {
   if (event.target.id === "email") {
-    
+event.preventDefault()
 
     const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
     const valid = regexEmail.test(event.target.value)
@@ -28,19 +31,31 @@ const onBlurHandler = (event) => {
   } 
 }
 
+//this validates that the message entry has text in it
+const onBlurdHandler = (event) => {
 
-  function isExpanded() {
+  if (event.target.id === "textEntry") {
+    const messageValue = event.target.value
+
+    if(messageValue === "") {
+      window.alert("Please enter a message")
+    }
+  }
+}
+
+  function connectForm() {
     if (props.currentPage === "Connect") {
       return (
       <div>
+        Want to connect?
       <Form>
   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-    <Form.Label>Email address</Form.Label>
+    <Form.Label>Email address:</Form.Label>
     <Form.Control onBlur={onBlurHandler} onChange= {onChangeHandler} id="email" type="email" placeholder="name@example.com" value={emailValue}/>
   </Form.Group>
   <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Example textarea</Form.Label>
-    <Form.Control onBlur={onBlurHandler} onChange= {onChangeHandler} id="textEntry" as="textarea" rows={3} value= {textValue}/>
+    <Form.Label>Message:</Form.Label>
+    <Form.Control onBlur={onBlurdHandler} onChange= {onChangeHandler} id="textEntry" as="textarea" rows={3} value= {textValue}/>
   </Form.Group>
 </Form>
 
@@ -51,7 +66,7 @@ const onBlurHandler = (event) => {
     }
   }
 
-  return <div>{isExpanded()}</div>;
+  return <div>{connectForm()}</div>;
 }
 
 export default Connect;
