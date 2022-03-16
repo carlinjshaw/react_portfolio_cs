1a81ebcbaeaf5b55feff7a33878730341e8ac5ef
carlinjshaw <carlinjshaw@gmail.com>
2022-03-16 12:08:51 -0400

    fixed resume page, embedded pdf, changed border colors

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import resume from '../assets/computer_teacher_resume_2021.pdf'
+import resume from '../assets/Carlin_Shaw_Developer_Resume.pdf'
 import firstImg from '../assets/first_img.PNG'
 
 function Resume(props) {
     function isExpanded() {
         if(props.currentPage === "Resume") {
             return (
-              <div>
-               Front-end Proficiencies:
-                HTML
-                CSS
-                Javascript
-                JQuery
-                Responsive design
-                React
-                Bootstrap
+              <div className="resume-page">
                 
+                  <h1></h1>
+                  <iframe className= 'resume'
+                    src={resume}
+                    width="50%"
+                    height="700px"
+                  ></iframe>
                 
-                Back-end Proficiencies:
-                APIs
-                Node
-                Express
-                MySql, Sequelize
-                MongoDB, Mongoose
-                REST
-                GraphQL
-                
-              <a href={resume}>Download Resume</a> 
-
-                
-              
-
               </div>
             );
         } else {
