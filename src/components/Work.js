92f727570b430e78360cc4600f0cf292186aa051
carlinjshaw <carlinjshaw@gmail.com>
2022-03-17 22:47:56 -0400

    all work link hidden until hover yay

--- src/components/Work.js
+++ src/components/Work.js
@@ -15,58 +15,83 @@ function Work(props) {
 
   
     return (
-      <div class = "workPage">
+      <div class="workPage">
         <div class="cards-container">
           <div className="item">
-              <h4>MVC Blog</h4> 
-              
-              
-              Uses Sequelize, Express, MVC format. 
-            <img class="projectImages" src={mvc}/>
-          
-              <a className= ""href="https://calm-basin-00430.herokuapp.com/"><img className='work-mvs-link' src={linkImage}></img></a> 
-              
-              <a href="https://github.com/carlinjshaw/MVC-cshaw"><img className='work-mvs-git' src={githubSymbol}></img></a> 
-          
+            <h4>MVC Blog</h4>
+            Uses Sequelize, Express, MVC format.
+            <img class="projectImages" src={mvc} />
+            <a href="https://calm-basin-00430.herokuapp.com/">
+              <img className="work-link" src={linkImage}></img>
+            </a>
+            <a href="https://github.com/carlinjshaw/MVC-cshaw">
+              <img className="work-git" src={githubSymbol}></img>
+            </a>
           </div>
-          <div class="item">
+          <div className="item">
             <h4>React Game</h4>
-            <a href="https://crypt-crawler.herokuapp.com/">Site</a>
-            <a href="https://github.com/Salas4969/Project-3-Crypt-Crawler">Github</a>
-
             Uses React, Heroku, Mongoose, GraphQL
             <img class="projectImages" src={cryptGame} />
+                  <a href="https://crypt-crawler.herokuapp.com/">
+                    <img className="work-link" src={linkImage}></img>
+                  </a>
+                  <a href="https://github.com/carlinjshaw/MVC-cshaw">
+                    <img className="work-git" src={githubSymbol}></img>
+                  </a>
+
           </div>
           <div class="item">
             <h4>Password Generator</h4>
-
-            <a href="https://carlinjshaw.github.io/03-password/">Site</a>
-            <a href="https://github.com/carlinjshaw/03-password">Github</a>
+            
             Uses Javascript
             <img class="projectImages" src={passwordGenerator} />
+
+            <a href="https://carlinjshaw.github.io/03-password/">
+                    <img className="work-link" src={linkImage}></img>
+                  </a>
+                  <a href="https://github.com/carlinjshaw/03-password">
+                    <img className="work-git" src={githubSymbol}></img>
+                  </a>
+
           </div>
           <div class="item">
             <h4>SQL Employee Manager</h4>
-
-            <a href="https://github.com/carlinjshaw/sql-employee-manager-cshaw">Github</a>
-
-            Uses MySql, Inquirer 
+            
+            Uses MySql, Inquirer
             <img class="projectImages" src={sqlProject} />
+
+            
+                  <a href="https://github.com/carlinjshaw/sql-employee-manager-cshaw">
+                    <img style= {{left: "50%"}} className="work-git" src={githubSymbol}></img>
+                  </a>
+
           </div>
           <div class="item">
             <h4>Workout Tracker API</h4>
-
-            <a href="https://damp-river-50096.herokuapp.com/">Site</a>
-            <a href="https://github.com/Browntuckerr/FullStackWorkout">Github</a>
+            
             Uses Sequelize, Node.js, Express, Handlebars
             <img class="projectImages" src={workoutProject} />
+
+            <a href="https://damp-river-50096.herokuapp.com/">
+                    <img className="work-link" src={linkImage}></img>
+                  </a>
+                  <a href="https://github.com/Browntuckerr/FullStackWorkout">
+                    <img className="work-git" src={githubSymbol}></img>
+                  </a>
+
           </div>
           <div class="item">
             <h4>Weather API</h4>
-            <a href="https://carlinjshaw.github.io/cshaw-weather-app/">Site</a>
-            <a href="https://github.com/carlinjshaw/cshaw-weather-app">Github</a>
+            
             Uses 3rd party APIs, Javasript, CSS.
             <img class="projectImages" src={weather} />
+
+            <a href="https://carlinjshaw.github.io/cshaw-weather-app/">
+                    <img className="work-link" src={linkImage}></img>
+                  </a>
+                  <a href="https://github.com/carlinjshaw/cshaw-weather-app">
+                    <img className="work-git" src={githubSymbol}></img>
+                  </a>
           </div>
         </div>
       </div>
